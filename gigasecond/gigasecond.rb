7939e1d0f8324c87db7132092df63b4895d50427
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-01-24 16:54:12 -0500

    refactored the Hamming.rb file to be more portable and to have shorter methods

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -1,8 +1,12 @@
 module Gigasecond
 
-  def self.from(time)
-    gs = time.to_time + 10**9
-    gs.to_date
+  def self.gigasecond
+    10 ** 9
+  end
+
+  def self.from(birthday)
+    gs_anniversary = birthday.to_time + self.gigasecond
+    gs_anniversary.to_date
   end
 
 end
