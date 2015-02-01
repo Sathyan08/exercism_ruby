2e6f00e6705b05eb77a2906b3efc03e8a840a1fc
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-02-01 16:10:39 -0500

    Solved the grains problem

--- leap/year.rb
+++ leap/year.rb
@@ -1,23 +1,19 @@
 module Year
 
   def self.leap?(year)
-    self.year_check(year)
-  end
-
-  def self.year_check(year)
-    self.four_century_check?(year)
+    self.four_century_check?(year) || (self.one_centure_check?(year) && self.four_year_check?(year))
   end
 
   def self.four_century_check?(year)
-    (year % 400 == 0) ? true : self.one_centure_check?(year)
+    year % 400 == 0
   end
 
   def self.one_centure_check?(year)
-    (year % 100 == 0) ? false : self.four_year_check?(year)
+    year % 100 != 0
   end
 
   def self.four_year_check?(year)
-    (year % 4 == 0) ? true : false
+    year % 4 == 0
   end
 
 end
