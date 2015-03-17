7884e6d026e75fc8e0f45a932941b2ab0855a58a
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-03-16 22:57:56 -0400

    wrote question class for bob.rb

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,16 +1,45 @@
 class Bob
 
+  def hey(remark)
+
+  end
+
 
 end
 
-class Question
+class Statement
 
   def new(input)
     @string = input
   end
 
   def type
-    silence? ? :silence : speech_test
+    return :silence if silence?
+    return :yelling if yelling?
+    return :question if question?
+    :statement
+  end
+
+  private
+
+  def silence?
+    @string.blank?
+  end
+
+  def yelling?
+    has_capitalized? && all_caps?
+  end
+
+  def question?
+    @string.chars.last == '?'
+  end
+
+  def has_capitalized?
+    @string.scan(/A-Z/).count > 0
+  end
+
+  def all_caps
+    @string.upcase == @string
   end
 
 end
